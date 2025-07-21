042c39f95e6190d4339d5521a83774d8053d25fb
lkdlkd <khanhdang2440@gmail.com>
2025-07-21 22:02:15 +0700

    40

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -10,7 +10,8 @@ export default function NotFoundPage() {
             <div className="text-center">
               <h2 className="d-flex justify-content-center align-items-center gap-2 mb-4">
                 <span className="display-1 fw-bold">4</span>
-                <i className="bi bi-exclamation-circle-fill text-danger display-4"></i>
+                <span className="display-1 fw-bold">0</span>
+                {/* <i className="bi bi-exclamation-circle-fill text-danger display-4"></i> */}
                 <span className="display-1 fw-bold bsb-flip-h">4</span>
               </h2>
               <h3 className="h2 mb-2">Oops! You're lost.</h3>
